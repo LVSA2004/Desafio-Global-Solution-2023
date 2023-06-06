23d70d0db725d3731119a0105ddae0f71fa3757a
Luan Sá <101113009+LVSA2004@users.noreply.github.com>
2023-06-06 19:38:00 -0300

    Delete GSJava directory

--- GSJava/src/main/java/br/com/fiap/gsjava/models/TokenJwt.java
+++
@@ -1,3 +0,0 @@
-package br.com.fiap.gsjava.models;
-
-public record TokenJwt(String token){}
--- GSJava/src/main/java/br/com/fiap/gsjava/repositories/PessoaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.gsjava.repositories;
-
-import br.com.fiap.gsjava.models.PessoaFisica;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface PessoaRepository extends JpaRepository<PessoaFisica, Long> {
-
-}
